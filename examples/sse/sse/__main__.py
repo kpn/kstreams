7301490a2a3bf6a10beaa094a6efab8526bf9a0d
Marcos Schroh <marcos.schroh@kpn.com>
2022-08-01 14:55:52 +0200

    build: sse example fixed

--- examples/sse/sse/__main__.py
+++ examples/sse/sse/__main__.py
@@ -1,18 +1,13 @@
 import logging
-
 import uvicorn
 
 
-def main():
+if __name__ == "__main__":
     uvicorn.run(
-        app="src.app:application",
+        app="sse.app:app",
         host="localhost",
         port=8000,
         log_level=logging.INFO,
         reload=True,
         debug=True,
     )
-
-
-if __name__ == "__main__":
-    main()
