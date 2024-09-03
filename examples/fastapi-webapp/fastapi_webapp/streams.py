f31a3a3317cda8e734500f28b5b0caafc221da63
Marcos Schroh <2828842+marcosschroh@users.noreply.github.com>
2024-09-03 16:14:22 +0200

    feat: error policy STOP_APPLICATION added. Closes #162 (#207)

--- examples/fastapi-webapp/fastapi_webapp/streams.py
+++ examples/fastapi-webapp/fastapi_webapp/streams.py
@@ -8,3 +8,6 @@ logger = logging.getLogger(__name__)
 @stream("local--kstream", group_id="kstreams--group-id")
 async def consume(cr: ConsumerRecord):
     print(f"Event consumed: headers: {cr.headers}, payload: {cr.value}")
+
+    if cr.value == b"error":
+        raise ValueError("error....")
--- tests/test_streams_error_policy.py
+++ tests/test_streams_error_policy.py
@@ -85,6 +85,41 @@ async def test_stop_engine_error_policy(stream_engine: StreamEngine):
 
 
 @pytest.mark.asyncio
+async def test_stop_application_error_policy(stream_engine: StreamEngine):
+    event = b'{"message": "Hello world!"}'
+    topic = "kstrems--local"
+    topic_two = "kstrems--local-two"
+    save_to_db = mock.Mock()
+    client = TestStreamClient(stream_engine)
+
+    with mock.patch("signal.raise_signal"):
+        @stream_engine.stream(topic, error_policy=StreamErrorPolicy.STOP_APPLICATION)
+        async def my_stream(cr: ConsumerRecord):
+            raise ValueError("Crashing Stream...")
+
+        @stream_engine.stream(topic_two)
+        async def my_stream_two(cr: ConsumerRecord):
+            save_to_db(cr.value)
+
+        async with client:
+            # send event and crash the first Stream, then the second one
+            # should be stopped because of StreamErrorPolicy.STOP_ENGINE
+            await client.send(topic, value=event, key="1")
+
+            # Send an event to the second Stream, it should be consumed
+            # as the Stream has been stopped
+            await client.send(topic_two, value=event, key="1")
+
+            # Both streams are stopped before leaving the context
+            assert not my_stream.running
+            assert not my_stream_two.running
+
+        # check that the event was consumed only once.
+        # The StreamEngine must wait for graceful shutdown
+        save_to_db.assert_called_once_with(b'{"message": "Hello world!"}')
+
+
+@pytest.mark.asyncio
 async def test_restart_stream_error_policy(stream_engine: StreamEngine):
     event = b'{"message": "Hello world!"}'
     topic = "kstrems--local-kskss"
